ba8e49719bad272d0354d1c13de9686d15f926ea
Davste93 <davste93@gmail.com>
2016-02-12 20:34:13 +0100

    Update example.ts

--- src/examples/example.ts
+++ src/examples/example.ts
@@ -66,26 +66,33 @@ userListSvc.addItem(newUser).then(function(user){
 });
 
 userListSvc.getAll().then(users => {
-  var user = users[0];
+  //This returns a List<UserWithAPI>
+  var user = users[0]; //So once we have one user, 
 
-  user.model.name = "Daniel";
- user.otherCustomMethod();
+  user.model.name = "Daniel"; //we can change the model
+ user.otherCustomMethod(); //Call any methods that extend our model 
 
-  user.save();
+  user.save(); //and make a put request directly from here to update it.
 });
 
 
 
 
 
-// Just an example, we need to work on this
+// Just an example, we need to work on this. There are many workarounds here because of generic type erasure:
+// We cannot instantiate an instance of a type in the class without passing it directly as type info is erased
+// in compile time. 
 class APIService {
+  //UserModelApi is optional, but passing it here means we can extend it and do stuff like call otherCustomMethod 
+  //without doing any casting.
   private static _UserListApiService : RequestApiModelList<UserModel, UserModelApi>  = null;
   static get UserListApiService() : RequestApiModelList<UserModel, UserModelApi> {
     return APIService._UserListApiService;
   }
 
   public static init(){
-    APIService._UserListApiService = new RequestApiModelList<UserModel, UserModelApi>(UserModel,  UserModelApi, UserModelApi.getBaseUrl())
+    APIService._UserListApiService = new RequestApiModelList<UserModel, UserModelApi>(
+      UserModel,  UserModelApi, UserModelApi.getBaseUrl()) //UserModel, UserModelApi should not be necessary here, but passing type in constructor
+      // seems to be the best workaround http://stackoverflow.com/questions/17382143/how-to-create-a-new-object-from-type-parameter-in-generic-class-in-typescript
   }
 }
